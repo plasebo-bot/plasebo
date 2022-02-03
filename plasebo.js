bab3926f9523af936bb87912298ac856c08dcfe8
Furkan Çentek <56794915+rootcf@users.noreply.github.com>
2022-02-03 06:17:48 +0300

    OwnerId Removed

--- plasebo.js
+++ plasebo.js
@@ -1,7 +1,7 @@
 const { Client, Intents, Collection, Discord, Interaction, MessageEmbed } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
 const { getCommands} = require("./utils/getCommands.js")
-const { token, prefix, ownerid } = require("./config.json")
+const { token, prefix } = require("./config.json")
 const {
     login
 } = require("./events/client.login.js")
@@ -14,5 +14,5 @@ const {
 
 getCommands(client);
 readyClient(client,prefix);
-check(client, prefix,ownerid);
+check(client, prefix);
 login(token, client)
